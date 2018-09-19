6db3d87a7680f3800a315e0e9a802c83ba0639f4
Chon Sou <sou.chon@outlook.com>
2018-09-19 22:46:13 +0100

    curation of ukbiobank incoming stream WIP

--- packages/itmat-ukb-curator/src/controllers/storageEngine.ts
+++ packages/itmat-ukb-curator/src/controllers/storageEngine.ts
@@ -2,16 +2,42 @@
 import express from 'express';
 import Papa from 'papaparse';
 import multer from 'multer';
+import { Database } from 'itmat-utils';
+import { UKBFields, FieldEntry } from '../curation/UKBFields';
+import { UKBCoding } from '../curation/UKBCoding';
 
 const parseOptions: Papa.ParseConfig = {
     delimiter: ',',
     quoteChar: '"',
     header: false,
     trimHeaders: true,
-    dynamicTyping: true,
     encoding: "utf-8",
 };
 
+interface fieldDescription {
+    fieldId: number,
+    instance: number,
+    array: number
+}
+
+interface headerArrayElement {
+    fieldDescription: fieldDescription,
+    metadata?: {
+        coding: undefined | number,
+        valueType: string
+    }
+}
+
+const UKBiobankValueTypes = {   // 'date' are provided in yyyy-mm-dd;
+    Integer: 'integer',
+    'Categorical single': 'categorical single',
+    'Categorical multiple': 'categorical multiple',
+    Compound: 'compound',
+    Date: 'date',
+    Text: 'text',
+    Continuous: 'float',
+    Time: 'date'
+}
 
 class _CSVStorageEngine implements multer.StorageEngine {
     public _handleFile(req: express.Request, file: any, cb: (error?: any, info?: object) => void): void {
@@ -20,25 +46,95 @@ class _CSVStorageEngine implements multer.StorageEngine {
         let numOfSubj = 0;
         let startTime: number;
         let endTime: number;
-        let header: string[];
-        const parseStream: NodeJS.ReadableStream = incomingStream.pipe(Papa.parse(Papa.NODE_STREAM_INPUT, parseOptions));
+        let fieldNumber: number;
+        const fieldsWithError: string[] = [];
+        const header: headerArrayElement[] = [{ fieldDescription: {fieldId: -999999, instance: -999999, array: -999999}} ];  //the first element is subject id
+        const parseStream: NodeJS.ReadableStream = incomingStream.pipe(Papa.parse(Papa.NODE_STREAM_INPUT, parseOptions)); //piping the incoming stream to a parser stream
 
-        parseStream.on('data', line => {
+        parseStream.on('data', async (line: string[]) => {
             if (isHeader) {
                 startTime = Date.now();
+                /* pausing the stream so all the async ops on the first line must be completed before the second line is read */
+                parseStream.pause();
+                fieldNumber = line.length; //saving the fieldNum to check each line has the same #column
+                const promiseArr = [];
                 isHeader = false;
-                console.log('header', line[0], line.length);
-                header = line;
+                for (let i = 1; i < line.length; i++) { //starting from the second column
+                    const el = line[i];
+                    const fieldDescription = {    //add in data provenance and user too;
+                        fieldId: parseInt(el.slice(0, el.indexOf('-'))),
+                        instance: parseInt(el.slice(el.indexOf('-')+1, el.indexOf('.'))),
+                        array: parseInt(el.slice(el.indexOf('.')+1))
+                    };
+                    header.push({fieldDescription});
+                    promiseArr.push(UKBFields.getFieldInfo(fieldDescription.fieldId));
+                }
+                const results = await Promise.all(promiseArr);  //all promises need to be resolved before resuming stream because the following rows of csv depend on info of the header
+                for(let j = 0; j < results.length; j++) {
+                    if (results[j] && results[j].Instances >= header[j+1].fieldDescription.instance && results[j].Array >= header[j+1].fieldDescription.array) { //making sure the fieldid in the csv file is not bogus
+                        const valueType: string = results[j].ValueType;
+                        const coding: undefined | number = results[j].Coding === null ? undefined : (results[j].Coding as number);
+                        header[j+1].metadata = {
+                            coding,
+                            valueType: (UKBiobankValueTypes as any)[valueType]
+                        }
+                    } else {
+                        fieldsWithError.push(line[j+1]);
+                    }
+                };
+                parseStream.resume();  //now that all promises have resolved, we can resume the stream
             } else {
+                /* no need to pause stream here because the calls to database don't need to follow any order */
+                if (line.length !== fieldNumber) {
+                    throw Error('column number doesnt match');
+                }
+                const eid = parseInt(line[0]);
+                for (let i = 1; i < line.length; i++) {
+                    let entry;
+                    if (line[i] !== null && line[i] !== '') {
+                        if ((header[i].metadata as any).coding) {
+                            const meaning = await UKBCoding.getCodeMeaning((header[i].metadata as any).coding, line[i]);
+                            if (meaning !== null) {
+                                entry = {
+                                    eid,
+                                    ...header[i].fieldDescription,
+                                    value: meaning
+                                };
+                                await Database.UKB_data_collection.insertOne(entry);
+                            } else {
+                                if (isNaN(parseFloat(line[i]))) {
+                                    console.log('CANNOT FIND MEANING', {
+                                        eid,
+                                        ...header[i].fieldDescription,
+                                        value:line[i]
+                                    });
+                                } else {
+                                    entry = {
+                                        eid,
+                                        ...header[i].fieldDescription,
+                                        value: parseFloat(line[i])
+                                    };
+                                    await Database.UKB_data_collection.insertOne(entry);
+                                }
+                            }
+                        } else {
+                            entry = {
+                                eid,
+                                ...header[i].fieldDescription,
+                                value: ((header[i].metadata as any).valueType === 'integer' || (header[i].metadata as any).valueType === 'float') ? parseFloat(line[i]) : line[i]
+                            };
+                            await Database.UKB_data_collection.insertOne(entry);
+                        }
+                    }
+                }
                 numOfSubj++;
-                console.log(numOfSubj, line[0], line.length);
             }
         });
 
         parseStream.on('end', () => { 
             endTime = Date.now();
-            console.log(endTime-startTime);
-            cb(null, { numOfSubj }); 
+            console.log(endTime - startTime);
+            cb(null, { numOfSubj, fieldsWithError }); 
         } )
     }
 
--- packages/itmat-ukb-curator/src/curation/UKBData.ts
+++ packages/itmat-ukb-curator/src/curation/UKBData.ts
@@ -16,25 +16,4 @@ export interface DataEntry {
 
 export class UKBData {
 
-    public static async addOneDataEntry(entry: DataEntry, decode: boolean, strictlyAdd?: boolean): Promise<boolean> {
-        const { fieldId, instance, array, value } = entry;
-        const fieldInfo: FieldEntry = await UKBFields.getFieldInfo(fieldId);
-        if (entry.instance > fieldInfo.Instances || entry.array > fieldInfo.Array ) {  //checking if the csv conforms with info from ukb
-            return false;
-        }
-
-        if (decode && fieldInfo.Coding !== null) {  //if the user specifies for decoding and ukb info says it's coded
-            entry = await UKBCoding.decodeEntry(entry);
-        }
-
-        if (strictlyAdd) {
-            await Database.UKB_data_collection.insert(entry);
-        } else {
-            const criteria = { ...entry };
-            delete criteria.value;
-            await Database.UKB_data_collection.update(criteria, entry )
-        }
-        return true;
-        
-    }
 }
