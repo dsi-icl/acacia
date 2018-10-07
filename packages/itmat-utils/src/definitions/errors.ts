0e872fba739504e401dc3203a608a0a6b1049bda
Chon Sou <sou.chon@outlook.com>
2018-10-07 18:39:08 +0100

    added validationHelper and some tests for it

--- packages/itmat-utils/src/definitions/errors.ts
+++ packages/itmat-utils/src/definitions/errors.ts
@@ -1,9 +1,9 @@
 export const APIErrorTypes = {
-    entryNotFound: (entryName: TemplateStringsArray) => `Entry for ${entryName} not found.`,
+    entryNotFound: (entryName: string) => `Entry for ${entryName} not found.`,
     authorised: 'You are not authorised to be doing this request.',
-    missingQueryString: (queryParam: TemplateStringsArray) => `You must provide a query parameter '${queryParam}'.`,
+    missingQueryString: (queryParams: string[]) => `You must provide the following query parameters '${queryParams.join(',')}'.`,
     duplicateQueryString: (queryParam: TemplateStringsArray) => `You must provide only one instance of this query parameter: '${queryParam}'.`,
     invalidUKBFieldIDQueryString: 'fieldId must be a number; if you are copying the id from UK Biobank csv file, provide the only fieldId before \'-\'.',
     missingRequestKey: (object: string, missingKeys: string[]) => `A ${object} object with key(s) '${missingKeys.join(',')}' are expected. Please check API doc.`,
-    invalidReqKeyValue: (key: string, ...rest: string[]) => `Invalid value for key '${key}'. ${rest.length !== 0 ? `Valid value: ${rest.join(',')}` : '' }`
+    invalidReqKeyValue: (key: string, validValues: any[]) => `Invalid value for key '${key}'. ${validValues.length !== 0 ? `Valid value(s): ${validValues.join(',')}` : '' }`
 };
