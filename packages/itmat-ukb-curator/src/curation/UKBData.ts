b390aaf69929430dc9df2ea00ec6a95ac6a93750
Chon Sou <sou.chon@outlook.com>
2018-10-11 18:30:23 +0100

    [UKB] moved over some functionalities

--- packages/itmat-ukb-curator/src/curation/UKBData.ts
+++ packages/itmat-ukb-curator/src/curation/UKBData.ts
@@ -1,13 +1,25 @@
 import csvparse from 'csv-parse';
+import { UKBCurationDatabase } from '../database/database';
+import { ICodingMap } from '../models/UKBCoding';
+import { IFieldMap } from '../models/UKBFields';
 
-export interface DataEntry {
-    patientId: string,
-    study: string,
-    fieldId: number,
-    instance: number,
-    array: number,
-    value: string | number
+interface headerArrayElement {
+    coding: undefined | ICodingMap,
+    valueType: string
 }
+
+
+const UKBiobankValueTypes: any = {   // 'date' are provided in yyyy-mm-dd;
+    Integer: 'integer',
+    'Categorical single': 'categorical single',
+    'Categorical multiple': 'categorical multiple',
+    Compound: 'compound',
+    Date: 'date',
+    Text: 'text',
+    Continuous: 'float',
+    Time: 'date'
+}
+
 
 /* update should be audit trailed */ 
 
@@ -27,10 +39,95 @@ export class UKBDataCurator {
     }
 
     public async processIncomingStreamAndUploadToMongo() {
+        let isHeader: boolean = true;
+        let fieldNumber: number;
+        const fieldDict = UKBCurationDatabase.FIELD_DICT;
+        let numOfSubj = 0;
+        let lineNum: number = 0;
+        const fieldsWithError: number[] = [];
+        const header: (headerArrayElement|undefined)[] = [ undefined ];  //the first element is subject id
+        let bulkInsert = UKBCurationDatabase.UKB_data_collection.initializeUnorderedBulkOp();
         const parseStream: NodeJS.ReadableStream = this.incomingWebStream.pipe(csvparse(this.parseOptions)); //piping the incoming stream to a parser stream
+
         //check for intergrity
-        parseStream.on('data', (data) => {
-            console.log(data);
+        parseStream.on('data', (line) => {
+            if (isHeader) {
+                /* pausing the stream so all the async ops on the first line must be completed before the second line is read */
+                parseStream.pause();
+                fieldNumber = line.length; //saving the fieldNum to check each line has the same #column
+                isHeader = false;
+                for (let i = 1; i < line.length; i++) { //starting from the second column
+                    let fieldToBeAdded: any = {};
+                    const el = line[i];
+                    const fieldDescription = {    //add in data provenance and user too;
+                        fieldId: parseInt(el.slice(0, el.indexOf('-'))),
+                        instance: parseInt(el.slice(el.indexOf('-')+1, el.indexOf('.'))),
+                        array: parseInt(el.slice(el.indexOf('.')+1))
+                    };
+                    const key = `${fieldDescription.fieldId}-${fieldDescription.instance}-${fieldDescription.array}`;
+                    const fieldInfo = (fieldDict as IFieldMap)[fieldDescription.fieldId];
+                    if (fieldInfo
+                            && fieldInfo.Instances >= fieldDescription.instance
+                            && fieldInfo.Array >= fieldDescription.array) { //making sure the fieldid in the csv file is not bogus
+                        const valueType: string = fieldInfo.ValueType;
+                        if (fieldInfo.Coding && (UKBCurationDatabase.CODING_DICT as ICodingMap)[fieldInfo.Coding]) {
+                            fieldToBeAdded = {
+                                coding: (UKBCurationDatabase.CODING_DICT as ICodingMap)[fieldInfo.Coding],
+                                valueType: UKBiobankValueTypes[valueType],
+                                fieldId: key
+                            };
+                            header.push(fieldToBeAdded);
+                        } else {
+                            fieldToBeAdded = {
+                                valueType: UKBiobankValueTypes[valueType],
+                                fieldId: key
+                            };
+                            header.push(fieldToBeAdded);
+                        }
+                    } else {
+                        header.push(undefined);
+                        fieldsWithError.push(fieldDescription.fieldId);
+                    }
+                }
+                parseStream.resume();  //now that all promises have resolved, we can resume the stream
+            } else {
+                console.log('running second line');
+                /* no need to pause stream here because the calls to database don't need to follow any order */
+                if (line.length !== fieldNumber) {
+                    throw Error('column number doesnt match');
+                }
+                const eid = line[0];
+                const entry: any = { eid };
+                for (let i = 1; i < line.length; i++) {
+                    if (line[i] !== null && line[i] !== '' && header[i] !== undefined) {
+                        if ((header[i] as any).coding) {
+                            const meaning = (header[i] as any).coding[line[i]];
+                            if (meaning !== undefined) {
+                                entry[(header[i] as any).fieldId] = meaning;
+                            } else {
+                                if (isNaN(parseFloat(line[i]))) {
+                                    console.log('CANNOT FIND MEANING', {
+                                    });
+                                } else {
+                                    entry[(header[i] as any).fieldId] = parseFloat(line[i]);
+                                }
+                            }
+                        } else {
+                            entry[(header[i] as any).fieldId] = ((header[i] as any).valueType === 'integer' || (header[i] as any).valueType === 'float') ? parseFloat(line[i]) : line[i];
+                        }
+                    }
+                }
+                bulkInsert.insert(entry);
+                numOfSubj++;
+                console.log('lineNum', ++lineNum)
+                if (numOfSubj > 2000) {     //race condition?   //PROBLEM: the last bit <2000 doesn't get uploaded\
+                    numOfSubj = 0;
+                    bulkInsert.execute((err: Error, result) => {
+                        if (err) { console.log((err as any).writeErrors[1].err); return; };
+                    });
+                    bulkInsert = UKBCurationDatabase.UKB_data_collection.initializeUnorderedBulkOp();
+                }
+            }
         });
 
         parseStream.on('end', () => {
