9c586a0eb7dd5f4d8a0fe534eb59887e73ec40f1
pas-de-sou-c <sou.chon@outlook.com>
2019-01-22 14:13:02 +0000

    [CLIE] change password to type='password'

--- packages/itmat-client/src/components/users/createNewUser.tsx
+++ packages/itmat-client/src/components/users/createNewUser.tsx
@@ -25,7 +25,7 @@ export const CreateNewUser: React.FunctionComponent = props => {
                 <h4>Create New User</h4>
                 <form>
                     <label>Username: </label><input type='text' value={inputs.username} onChange={e => { setInputs({...inputs, username: e.target.value }); }}/> <br/><br/>
-                    <label>Password: </label><input type='text' value={inputs.password} onChange={e => { setInputs({...inputs, password: e.target.value }); }}/> <br/><br/>
+                    <label>Password: </label><input type='password' value={inputs.password} onChange={e => { setInputs({...inputs, password: e.target.value }); }}/> <br/><br/>
                     <label>Real name: </label><input type='text' value={inputs.realName} onChange={e => { setInputs({...inputs, realName: e.target.value }); }}/> <br/><br/>
                     <label>Email notification: </label><input type='checkbox' checked={inputs.emailNotificationsActivated} onChange={e => { setInputs({...inputs, emailNotificationsActivated: e.target.checked }); }}/> <br/><br/>
                     <label>Email: </label><input type='text' value={inputs.email} onChange={e => { setInputs({...inputs, email: e.target.value }); }}/> <br/><br/>
