f46818b43d5cf1942d881d02ee5e02210baa418d
pas-de-sou-c <sou.chon@outlook.com>
2019-03-29 15:00:44 +0000

    [CLIE] field tree

--- packages/itmat-client/src/components/query/fields.tsx
+++ packages/itmat-client/src/components/query/fields.tsx
@@ -1,16 +1,53 @@
 import { Models } from 'itmat-utils';
 import * as React from 'react';
-import { Query } from "react-apollo";
-import * as css from '../../css/studyPage.module.css';
-import { GET_STUDIES_LIST } from '../../graphql/study';
-import { NavLink } from 'react-router-dom';
-import { WHO_AM_I } from '../../graphql/user';
+import { Tree, Input } from 'antd';
+const { TreeNode } = Tree;
+const { Search } = Input;
+import 'antd/lib/tree/style/css';
+import '../../css/antdOverride.css';
 
 export const FieldListSection: React.FunctionComponent<{ fieldList: Models.Field.IFieldEntry[] }> = ({ fieldList }) => {
+    // const sortedList = fieldList.sort((a, b) => `${a.Path} > ${a.Field}`.localeCompare(`${b.Path} > ${b.Field}`));
+    const transformedList = fieldList.map(el => `${el.Path} > ${el.FieldID}|${el.Field}`);
+
+    const makeTree = (paths: string[]) => {
+        const output: any = [];
+        for (let i = 0; i < paths.length; i++) {
+            const currentPath = paths[i].split(' > ');
+            let currentNode = output;
+            for (let j = 0; j < currentPath.length; j++) {
+                const wantedNode = currentPath[j];
+                const filteredNode = currentNode.filter((el: any) => el.name === wantedNode);
+                if (filteredNode.length === 1) {
+                    currentNode = filteredNode[0].children;
+                } else {
+                    const newNode = j === currentPath.length - 1 ? { fieldId: wantedNode.split('|')[0], name: wantedNode.split('|')[1], children: [] } : { name: wantedNode, children: [] };
+                    currentNode.push(newNode);
+                    currentNode = newNode.children;
+                }
+            }
+        }
+        return output;
+    };
+
+
+    const renderTreeNodes = (fieldList: any[]) => fieldList.map(item => {
+        if (item.children.length !== 0) {
+            return (
+            <TreeNode title={item.name} key={item.name} dataRef={item} isLeaf={false} selectable={false}>
+                {renderTreeNodes(item.children)}
+            </TreeNode>
+            );
+        }
+        return <TreeNode title={item.name} key={item.fieldId} dataRef={item} isLeaf={true} selectable={false} />;
+    });
+
     return (
         <>
         <h4>Available Fields</h4>
-        {fieldList.map(el => <div>{`${el.FieldID}: ${el.Field}`}</div>)}
+        <Tree.DirectoryTree>
+            {renderTreeNodes(makeTree(transformedList))}
+        </Tree.DirectoryTree>
         </>
     );
 };
