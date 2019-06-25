ab897a0f3aa359126f03964eccabf764009e229e
sou-chon <sou.chon@outlook.com>
2019-06-25 20:17:53 +0100

    logging

--- packages/itmat-interface/src/graphql/resolvers/index.ts
+++ packages/itmat-interface/src/graphql/resolvers/index.ts
@@ -15,12 +15,22 @@ const modules = [
     dataResolvers
 ];
 
+const loggingDecorator = (reducerFunction: Function) => {
+    return (parent: any, args: any, context: any, info: any) => {
+        console.log(reducerFunction.name, args, context.req.user);
+        reducerFunction(parent, args, context, info);
+    };
+};
+
 const reduceInit: any = { JSON: GraphQLJSON };
 export const resolvers = modules.reduce((a, e) => {
     const types = Object.keys(e);
     for (const each of types) {
         if (a[each] === undefined) {
             a[each] = {};
+        }
+        for (const funcName of Object.keys((e as any)[each])) {
+            (e as any)[each][funcName] = loggingDecorator((e as any)[each][funcName]);
         }
         a[each] = { ...a[each], ...(e as any)[each] };
     }
