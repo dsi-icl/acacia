067dc78c94e4cfc3b40a44911739df0924811b95
Florian Guitton <f.guitton@imperial.ac.uk>
2019-09-17 16:02:21 +0100

    Adding Lerna build and run logic

--- packages/itmat-query-language/src/index.ts
+++ packages/itmat-query-language/src/index.ts
@@ -1,11 +1,11 @@
-import { Lexer } from './lexer';
-import { Parser } from './parser';
-import { CST2IR } from './cst2ir';
-import { Pipeline } from './generatePipeline';
+import { Lexer } from './lexer/lexer';
+import { Parser } from './parser/parser';
+import { CST2IR } from './parser/cst2ir';
+// import { Pipeline } from './generatePipeline';
 
 const string = 'value (field "Sex") = "Male" OR ( value (field "Age") = 30 AND value(field "Sex") = "Female" )';
 
 const lexer = new Lexer(string);
 const parser = new Parser(lexer.tokenize(true));
 const cst2ir = new CST2IR(parser.parse()).toIR();
-const pipeline = new Pipeline(cst2ir).generateMongoPipeline();
+// const pipeline = new Pipeline(cst2ir).generateMongoPipeline();
