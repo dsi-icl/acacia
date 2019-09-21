683459b6e17864ef21fa722d8c27299da2af3304
sou-chon <sou.chon@outlook.com>
2019-09-21 19:23:42 +0100

    project tests (WIP)

--- packages/itmat-ui-react/cypress/integration/studies.js
+++ packages/itmat-ui-react/cypress/integration/studies.js
@@ -11,4 +11,32 @@ describe('Studies page', function() {
         cy.contains('Status');
         cy.contains('Metadata');
     });
+
+    it.only('admin can create projects (e2e)', function() {
+        /* setup: login via API */
+        cy.request('POST', 'http://localhost:3003/graphql', LOGIN_BODY_ADMIN);
+        const studyId = '5f0e6362-e593-4d61-a2bc-73730d8933f6';
+        cy.visit(`/datasets/${studyId}/projects`);
+
+        /* clicking the create new user button to go to page */
+        cy.get('[placeholder="Enter name"]', { timeout: 100000 }).type('newprojecttest', { force: true });
+        // cy.contains('Add new project').click();
+    });
+
+
+
+    it.only('displays error message when creating new project if name is not provided (e2e)', function() {
+        /* setup: login via API */
+        cy.request('POST', 'http://localhost:3003/graphql', LOGIN_BODY_ADMIN);
+        const studyId = '5f0e6362-e593-4d61-a2bc-73730d8933f6';
+        cy.visit(`/datasets/${studyId}/projects`);
+
+        /* error bar should not be visible */
+        cy.contains('Add new project', { timeout: 100000 });   // making sure ajax is finished 
+        cy.contains('Please enter project name.').should('not.exist');
+
+        /* clicking the create new user button to go to page */
+        cy.contains('Add new project').click();
+        cy.contains('Please enter project name.').should('have.class', 'error_banner');
+    });
 });
