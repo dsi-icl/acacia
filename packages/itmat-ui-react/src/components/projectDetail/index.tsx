39021fada0995910fca4ebca11ef05fcb1f13eae
Florian Guitton <f.guitton@imperial.ac.uk>
2019-10-25 17:42:35 +0100

    Hidding non implemented sections

--- packages/itmat-ui-react/src/components/projectDetail/index.tsx
+++ packages/itmat-ui-react/src/components/projectDetail/index.tsx
@@ -22,7 +22,7 @@ export const ProjectDetailPage: React.FunctionComponent<{ projectId: string }> =
                     <div className={css.tabs}>
                         <div>
                             <NavLink to={`/projects/${projectId}/dashboard`} activeClassName={css.active}><div>DASHBOARD</div></NavLink>
-                            <NavLink to={`/projects/${projectId}/samples`} activeClassName={css.active}><div>SAMPLE</div></NavLink>
+                            {/* <NavLink to={`/projects/${projectId}/samples`} activeClassName={css.active}><div>SAMPLE</div></NavLink> */}
                             <NavLink to={`/projects/${projectId}/data`} activeClassName={css.active}><div>DATA</div></NavLink>
                             <NavLink to={`/projects/${projectId}/files`} activeClassName={css.active}><div>FILE REPOSITORY</div></NavLink>
                             <NavLink to={`/projects/${projectId}/admin`} activeClassName={css.active}><div>ADMINISTRATION</div></NavLink>
@@ -32,7 +32,7 @@ export const ProjectDetailPage: React.FunctionComponent<{ projectId: string }> =
                         <Switch>
                             <Route path="/projects/:projectId/dashboard" render={() => <DashboardTabContent studyId={data.getProject.studyId} jobs={data.getProject.jobs} />} />
                             <Route path="/projects/:projectId/admin" render={({ match }) => <AdminTabContent studyId={data.getProject.studyId} projectId={match.params.projectId} roles={data.getProject.roles} />} />
-                            <Route path="/projects/:projectId/samples" render={() => <></>} />
+                            {/* <Route path="/projects/:projectId/samples" render={() => <></>} /> */}
                             <Route path="/projects/:projectId/data" render={() => <DataTabContent studyId={data.getProject.studyId} projectId={projectId} />} />
                             <Route path="/projects/:projectId/files" render={() => <FileTabContent projectId={projectId} studyId={data.getProject.studyId} />} />
                             <Route path="/projects/:projectId/" render={() => <Redirect to={`/projects/${projectId}/dashboard`} />} />
