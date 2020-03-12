2303168a44c150304c84d817b7fe94e718975473
sou-chon <sou.chon@outlook.com>
2020-03-12 16:03:27 +0000

    permision refinement

--- packages/itmat-commons/src/index.ts
+++ packages/itmat-commons/src/index.ts
@@ -1,4 +1,4 @@
-export { permissions } from './permissions';
+export { permissions, task_required_permissions } from './permissions';
 export { GQLRequests } from './graphql/index';
 import * as Models from './models/index';
 export { Models };
