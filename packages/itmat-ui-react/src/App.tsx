7fa4be8636a0eedb75f6fbe279a17eb9b0ba22d7
Florian Guitton <f.guitton@imperial.ac.uk>
2020-04-02 16:30:23 +0100

    Moving providers to they own wrapper. Closes #441

--- packages/itmat-ui-react/src/App.tsx
+++ packages/itmat-ui-react/src/App.tsx
@@ -1,57 +1,48 @@
 import * as React from 'react';
-import { ApolloProvider, Query } from 'react-apollo';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Query } from 'react-apollo';
 import { WHO_AM_I } from 'itmat-commons/dist/graphql/user';
-import { client } from './components/apolloClient';
+import Providers from './Providers';
 import { LoginBox } from './components/login/login';
 import { Spinner } from './components/reusable/icons/spinner';
 import { MainMenuBar } from './components/scaffold/mainMenuBar';
 import { MainPanel } from './components/scaffold/mainPanel';
 import css from './components/scaffold/scaffold.module.css';
 
-class App extends React.Component {
-    public render() {
-        return (
-            <ApolloProvider client={client}>
-                <Query<any, any> query={WHO_AM_I}>
-                    {({ loading, error, data }) => {
-                        if (loading) {
-                            return (
-                                <div style={{
-                                    width: '100%', height: '100%', textAlign: 'center', paddingTop: '20%',
-                                }}
-                                >
-                                    <Spinner />
-                                </div>
-                            );
-                        }
-                        if (error) {
-                            return (
-                                <p>
-                                    Error :(
-                                    {' '}
-                                    {error.message}
-                                </p>
-                            );
-                        }
-                        if (data.whoAmI !== null && data.whoAmI !== undefined && data.whoAmI.username !== null) { // if logged in return the app
-                            return (
-                                <div className={css.app}>
-                                    <Router>
-                                        <>
-                                            <MainMenuBar projects={data.whoAmI.access.projects} />
-                                            <MainPanel />
-                                        </>
-                                    </Router>
-                                </div>
-                            );
-                        }
-                        return <LoginBox />; // if not logged in return the login boxs
-                    }}
-                </Query>
-            </ApolloProvider>
-        );
-    }
-}
+export const App: React.FC = () => (
+    <Providers>
+        <Query<any, any> query={WHO_AM_I}>
+            {({ loading, error, data }) => {
+                if (loading) {
+                    return (
+                        <div style={{
+                            width: '100%', height: '100%', textAlign: 'center', paddingTop: '20%',
+                        }}
+                        >
+                            <Spinner />
+                        </div>
+                    );
+                }
+                if (error) {
+                    return (
+                        <p>
+                            Error :(
+                            {' '}
+                            {error.message}
+                        </p>
+                    );
+                }
+                if (data.whoAmI !== null && data.whoAmI !== undefined && data.whoAmI.username !== null) { // if logged in return the app
+                    return (
+                        <div className={css.app}>
+                            <MainMenuBar projects={data.whoAmI.access.projects} />
+                            <MainPanel />
+                        </div>
+                    );
+                }
+                return <LoginBox />; // if not logged in return the login boxs
+            }}
+        </Query>
+    </Providers>
+);
 
 export default App;
---
+++ packages/itmat-ui-react/src/Providers.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { ApolloProvider, } from 'react-apollo';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { client } from './components/apolloClient';
+
+const Providers: React.FC = ({ children }) => (
+    <ApolloProvider client={client}>
+        <Router>
+            {children}
+        </Router>
+    </ApolloProvider>
+);
+
+export default Providers;
