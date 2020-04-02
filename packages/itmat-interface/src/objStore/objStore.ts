849820f2f5504a67291e65c0141f0f91389f1785
sou-chon <sou.chon@outlook.com>
2020-04-02 15:59:11 +0100

    switching to minio

--- packages/itmat-interface/src/objStore/objStore.ts
+++ packages/itmat-interface/src/objStore/objStore.ts
@@ -1,4 +1,4 @@
-import { OpenStackSwiftObjectStore } from 'itmat-utils';
+import { ObjectStore } from 'itmat-utils';
 import config from '../utils/configManager';
 
-export const objStore = new OpenStackSwiftObjectStore(config.swift);
+export const objStore = new ObjectStore(config.swift);
