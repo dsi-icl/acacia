c9aa582358ee91da3aae5b3c81e8d3da20193e01
sou-chon <sou.chon@outlook.com>
2020-04-05 20:05:59 +0100

    setup adding timeout to testing

--- packages/itmat-utils/test/setup-and-test.js
+++ packages/itmat-utils/test/setup-and-test.js
@@ -1,12 +1,19 @@
 const fetch = require('node-fetch');
 const child_process = require('child_process');
-const chalk = require('chalk');
+let chalk = require('chalk');
+chalk = new chalk.Instance({level: 1});
 
 const MINIO_PORT = 9000;
 
 try {
     child_process.execSync('docker --version', {stdio: 'inherit'});
+    child_process.execSync('docker pull minio/minio:latest', { stdio: 'inherit'});
 } catch (e) {
+    if (process.platform === 'darwin' || process.platform === 'win32') {
+        console.log(chalk.yellow('WARNING: Docker not found. If this is Windows or MacOS, only tests that do not require docker will be run. See linux for all tests.'));
+        // child_process.execSync('npm run jest --color --coverage ./test/serverTests/job.test.js ./test/serverTests/permission.test.js ./test/serverTests/study.test.js ./test/serverTests/users.test.js ./test/unitTests/permissionCore.test.js', { stdio: 'inherit'});
+        process.exit(0);
+    }
     console.error('ERROR SETTING UP TESTS: Docker not found. Object store integration tests require docker.');
     console.error(e);
     process.exit(1);
@@ -14,27 +21,30 @@ try {
 
 let minio;
 try {
-    child_process.execSync('docker pull minio/minio:latest', { stdio: 'inherit'});
-    minio = child_process.exec(`docker run --rm -p ${MINIO_PORT}:9000 minio/minio server /data`);
+    minio = child_process.exec(`docker run --name utils-minio --rm -p ${MINIO_PORT}:9000 minio/minio server /data`);
 } catch (e) {
     console.error(e);
-    if (minio) {
-        minio.kill();
-    }
+    child_process.execSync('docker stop utils-minio', { stdio: 'inherit' });
     process.exit(1);
 }
+
+const timeout = setTimeout(() => {
+    child_process.execSync('docker stop utils-minio', { stdio: 'inherit' });
+    process.exit(1);
+}, 1000 * 60 * 5 /* 5 min */);
 
 const fetchpoll = setInterval(() => {
     fetch(`http://localhost:${MINIO_PORT}`)
         .then(res => {
             if (res.status === 403) {
                 clearInterval(fetchpoll);
+                clearTimeout(timeout);
                 console.log(chalk.yellow('Docker minio has started up. Running tests now.'));
                 child_process.execSync('yarn test-integrate', { stdio: 'inherit' });
-                minio.kill();
+                child_process.execSync('docker stop utils-minio', { stdio: 'inherit' });
             } else {
                 console.error(`Unexpected res status ${res.status}`);
-                minio.kill();
+                child_process.execSync('docker stop utils-minio', { stdio: 'inherit' });
                 process.exit(1);
             }
         })
@@ -44,7 +54,7 @@ const fetchpoll = setInterval(() => {
             } else {
                 console.error(chalk.red('ERROR:'));
                 console.log(e);
-                minio.kill();
+                child_process.execSync('docker stop utils-minio', { stdio: 'inherit' });
                 process.exit(1);
             }
         });
