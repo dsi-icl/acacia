8a3cd7a60a2695f10222d337e0d4f83f374f616c
wsy19961129@live.com <wsy19961129@live.com>
2020-04-06 11:12:15 +0100

    abstract classes from CSVCurator.ts

--- packages/itmat-job-executor/src/curation/CSVCurator.ts
+++ packages/itmat-job-executor/src/curation/CSVCurator.ts
@@ -2,25 +2,7 @@ import csvparse from 'csv-parse';
 import { Collection } from 'mongodb';
 import { IJobEntry } from 'itmat-commons/dist/models/job';
 import { Writable } from 'stream';
-
-export interface IDataEntry {
-    m_jobId: string; // the id of the job that causes this upload
-    m_eid: string; // patient Id
-    m_study: string; // study Id
-    m_versionId: string; // data version Id 
-    [field: string]: {
-        [instance: string]: {
-            [array: number]: number | string
-        }
-    } | string | boolean | string[];
-}
-
-export interface IFieldDescriptionObject {
-    fieldId: number;
-    timepoint: number;
-    measurement: number;
-    datatype: 'c' | 'i' | 'd' | 'b' | 't';
-}
+import { IFieldDescriptionObject, IDataEntry } from '../utils/Interfaces';
 
 /* update should be audit trailed */
 /* eid is not checked whether it is unique in the file: this is assumed to be enforced by database */
