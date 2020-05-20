ba91f8dddec793a1931465f6f2e842fbcba9560b
sou-chon <sou.chon@outlook.com>
2020-05-20 16:05:06 +0100

    (hopefully) fixing the tests of users.tests

--- packages/itmat-interface/src/utils/configManager.ts
+++ packages/itmat-interface/src/utils/configManager.ts
@@ -4,6 +4,7 @@ import { IObjectStoreConfig } from 'itmat-utils';
 import { IDatabaseBaseConfig } from 'itmat-utils/dist/database';
 import configDefaults from '../../config/config.sample.json';
 import { IServerConfig } from '../server/server.js';
+import chalk from 'chalk';
 import { ProvidedRequiredArgumentsOnDirectivesRule } from 'graphql/validation/rules/ProvidedRequiredArgumentsRule';
 
 export interface INodemailerConfig {
@@ -27,30 +28,32 @@ interface IConfiguration extends IServerConfig {
 class ConfigurationManager {
 
     public static expand(configurationFile: string): IConfiguration {
+        let config: IConfiguration;
+        if (fs.existsSync(configurationFile)) {
 
-        try {
-            if (fs.existsSync(configurationFile)) {
+            const content = fs.readFileSync(configurationFile, 'utf8');
 
-                const content = fs.readFileSync(configurationFile, 'utf8');
-
-                const config = merge(configDefaults, JSON.parse(content));
-
-                if (process.env.CI === 'true') {
-                    if (process.env.TEST_SMTP_CRED) config.nodemailer.auth.pass = process.env.TEST_SMTP_CRED;
-                    if (process.env.TEST_SMTP_USERNAME) config.nodemailer.auth.user = process.env.TEST_SMTP_USERNAME;
-                    if (process.env.TEST_SMTP_HOST) config.nodemailer.host = process.env.TEST_SMTP_HOST;
-                    if (process.env.TEST_SMTP_PORT) config.nodemailer.port = parseInt(process.env.TEST_SMTP_PORT, 10);
-                }
-
-                return config;
+            try {
+                config = merge(configDefaults, JSON.parse(content));
+            } catch (e) {
+                console.error(chalk.red('Cannot parse configuration file. Using defaults.'));
+                config = configDefaults;
             }
-        } catch (e) {
-            console.error('Could not parse configuration file.');
+        } else {
+            console.error(chalk.red('Cannot find configuration file. Using defaults.'));
+            config = configDefaults;
         }
 
-        return configDefaults;
+        if (process.env.CI === 'true') {
+            if (process.env.TEST_SMTP_CRED) config.nodemailer.auth.pass = process.env.TEST_SMTP_CRED;
+            if (process.env.TEST_SMTP_USERNAME) config.nodemailer.auth.user = process.env.TEST_SMTP_USERNAME;
+            if (process.env.TEST_SMTP_HOST) config.nodemailer.host = process.env.TEST_SMTP_HOST;
+            if (process.env.TEST_SMTP_PORT) config.nodemailer.port = parseInt(process.env.TEST_SMTP_PORT, 10);
+        }
+
+        return config;
     }
 
 }
 
-export default ConfigurationManager.expand(process.env.TESTING === 'true' ? 'config/config.test.json' : 'config/config.json');
+export default ConfigurationManager.expand('config/config.json');
