4abfab15b40947a6b813b2a7f842537d65ded0d7
Florian Guitton <2495155+fguitton@users.noreply.github.com>
2020-06-04 15:03:30 +0000

    Changing ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,41 @@
 module.exports = {
-    extends: "eslint:recommended",
+    extends: 'eslint:recommended',
+    parser: '@typescript-eslint/parser',
+    plugins: [
+        '@typescript-eslint'
+    ],
     env: {
         es6: true,
         node: true
     },
+    ignorePatterns: [
+        '**/dist/**'
+    ],
+    rules: {
+        'indent': [
+            'error',
+            4,
+            { 'SwitchCase': 1 }
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ]
+    },
     overrides: [
         {
             files: [
-                "**/*.test.ts"
+                '*eslint*',
+                '*config.js',
+                'scripts/*'
+            ],
+            env: {
+                node: true
+            }
+        },
+        {
+            files: [
+                '**/*.test.ts'
             ],
             env: {
                 jest: true
@@ -16,13 +44,16 @@ module.exports = {
                 hasMinio: true,
                 minioContainerPort: true
             },
-            plugins: ["jest"],
+            plugins: [
+                'jest',
+                '@typescript-eslint'
+            ],
             rules: {
-                "jest/no-disabled-tests": "warn",
-                "jest/no-focused-tests": "error",
-                "jest/no-identical-title": "error",
-                "jest/prefer-to-have-length": "warn",
-                "jest/valid-expect": "error"
+                'jest/no-disabled-tests': 'warn',
+                'jest/no-focused-tests': 'error',
+                'jest/no-identical-title': 'error',
+                'jest/prefer-to-have-length': 'warn',
+                'jest/valid-expect': 'error'
             }
         }
     ],
