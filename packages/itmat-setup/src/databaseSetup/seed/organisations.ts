082b0a4877b41381cdf448cda500a79c8132c1b7
nguyentb <ngbitr@gmail.com>
2020-08-09 08:15:15 +0100

    update organisation collection field
    Signed-off-by: nguyentb <ngbitr@gmail.com>

--- packages/itmat-setup/src/databaseSetup/seed/organisations.ts
+++ packages/itmat-setup/src/databaseSetup/seed/organisations.ts
@@ -1,189 +1,236 @@
 export const seedOrganisations = [{
     id: 'organisation_101_replaced_at_runtime',
-    name: 'Abbvie Inc'
+    name: 'Abbvie Inc',
+    containOrg: null
 },
 {
     id: 'organisation_102_replaced_at_runtime',
-    name: 'Academisch Ziekenhuis Leiden'
+    name: 'Academisch Ziekenhuis Leiden',
+    containOrg: null
 },
 {
     id: 'organisation_103_replaced_at_runtime',
-    name: 'Asociaci—n Parkinson Madrid'
+    name: 'Asociaci—n Parkinson Madrid',
+    containOrg: null
 },
 {
     id: 'organisation_104_replaced_at_runtime',
-    name: 'AstraZeneca AB'
+    name: 'AstraZeneca AB',
+    containOrg: null
 },
 {
     id: 'organisation_105_replaced_at_runtime',
-    name: 'Biogen IDEC Ltd'
+    name: 'Biogen IDEC Ltd',
+    containOrg: null
 },
 {
     id: 'organisation_106_replaced_at_runtime',
-    name: 'Byteflies NV'
+    name: 'Byteflies NV',
+    containOrg: null
 },
 {
     id: 'organisation_107_replaced_at_runtime',
-    name: 'Cambridge Cognition Ltd'
+    name: 'Cambridge Cognition Ltd',
+    containOrg: null
 },
 {
     id: 'organisation_108_replaced_at_runtime',
-    name: 'CHDI Foundation'
+    name: 'CHDI Foundation',
+    containOrg: null
 },
 {
     id: 'organisation_109_replaced_at_runtime',
-    name: 'DREEM'
+    name: 'DREEM',
+    containOrg: null
 },
 {
     id: 'organisation_110_replaced_at_runtime',
-    name: 'ECRIN European Clinical Research Infrastructure Network'
+    name: 'ECRIN European Clinical Research Infrastructure Network',
+    containOrg: null
 },
 {
     id: 'organisation_111_replaced_at_runtime',
-    name: 'Eli Lilly & Co. Ltd'
+    name: 'Eli Lilly & Co. Ltd',
+    containOrg: null
 },
 {
     id: 'organisation_112_replaced_at_runtime',
-    name: 'Empirica Gesellschaft fŸr Kommunikations und Technologieforschung Mbh'
+    name: 'Empirica Gesellschaft fŸr Kommunikations und Technologieforschung Mbh',
+    containOrg: null
 },
 {
     id: 'organisation_113_replaced_at_runtime',
-    name: 'Erasmus Universitair Medisch Centrum Rotterdam'
+    name: 'Erasmus Universitair Medisch Centrum Rotterdam',
+    containOrg: null
 },
 {
     id: 'organisation_114_replaced_at_runtime',
     name: 'European Federation of CrohnÕs & Ulcerative Colitis Association',
+    containOrg: null
 },
 {
     id: 'organisation_115_replaced_at_runtime',
-    name: 'F. Hoffmann-La Roche AG'
+    name: 'F. Hoffmann-La Roche AG',
+    containOrg: null
 },
 {
     id: 'organisation_116_replaced_at_runtime',
-    name: 'FCincias.ID Ð Associa‹o para a Investiga‹o e Desenvolvimento de Cincias'
+    name: 'FCincias.ID Ð Associa‹o para a Investiga‹o e Desenvolvimento de Cincias',
+    containOrg: null
 },
 {
     id: 'organisation_117_replaced_at_runtime',
-    name: 'George-Huntington-Institut GmbH'
+    name: 'George-Huntington-Institut GmbH',
+    containOrg: null
 },
 {
     id: 'organisation_118_replaced_at_runtime',
-    name: 'Helse Stavanger Hf'
+    name: 'Helse Stavanger Hf',
+    containOrg: null
 },
 {
     id: 'organisation_119_replaced_at_runtime',
-    name: 'Imperial College of Science Technology and Medicine'
+    name: 'Imperial College of Science Technology and Medicine',
+    containOrg: null
 },
 {
     id: 'organisation_120_replaced_at_runtime',
-    name: 'Institut Mines-TŽlŽcom'
+    name: 'Institut Mines-TŽlŽcom',
+    containOrg: null
 },
 {
     id: 'organisation_121_replaced_at_runtime',
-    name: 'Instituto de Medicina Molecular Joao Lobo Antunes'
+    name: 'Instituto de Medicina Molecular Joao Lobo Antunes',
+    containOrg: null
 },
 {
     id: 'organisation_122_replaced_at_runtime',
-    name: 'Instytut Psychiatrii I Neurologii'
+    name: 'Instytut Psychiatrii I Neurologii',
+    containOrg: null
 },
 {
     id: 'organisation_123_replaced_at_runtime',
-    name: 'iXscient Ltd'
+    name: 'iXscient Ltd',
+    containOrg: null
 },
 {
     id: 'organisation_124_replaced_at_runtime',
-    name: 'Janssen Pharmaceutica NV'
+    name: 'Janssen Pharmaceutica NV',
+    containOrg: null
 },
 {
     id: 'organisation_125_replaced_at_runtime',
-    name: 'Lixoft SAS'
+    name: 'Lixoft SAS',
+    containOrg: null
 },
 {
     id: 'organisation_126_replaced_at_runtime',
-    name: 'McRoberts BV'
+    name: 'McRoberts BV',
+    containOrg: null
 },
 {
     id: 'organisation_127_replaced_at_runtime',
-    name: 'Medibiosense Ltd'
+    name: 'Medibiosense Ltd',
+    containOrg: null
 },
 {
     id: 'organisation_128_replaced_at_runtime',
-    name: 'Medizinische UniversitŠt Innsbruck'
+    name: 'Medizinische UniversitŠt Innsbruck',
+    containOrg: null
 },
 {
     id: 'organisation_129_replaced_at_runtime',
-    name: 'Orion OYJ'
+    name: 'Orion OYJ',
+    containOrg: null
 },
 {
     id: 'organisation_130_replaced_at_runtime',
-    name: 'ParkinsonÕs Disease Society of the United Kingdom'
+    name: 'ParkinsonÕs Disease Society of the United Kingdom',
+    containOrg: null
 },
 {
     id: 'organisation_131_replaced_at_runtime',
-    name: 'Pfizer Ltd'
+    name: 'Pfizer Ltd',
+    containOrg: null
 },
 {
     id: 'organisation_132_replaced_at_runtime',
-    name: 'Pluribus One S.r.l.'
+    name: 'Pluribus One S.r.l.',
+    containOrg: null
 },
 {
     id: 'organisation_133_replaced_at_runtime',
-    name: 'Queen Mary University of London'
+    name: 'Queen Mary University of London',
+    containOrg: null
 },
 {
     id: 'organisation_134_replaced_at_runtime',
-    name: 'Sanofi Aventis Recherche et DŽveloppement'
+    name: 'Sanofi Aventis Recherche et DŽveloppement',
+    containOrg: null
 },
 {
     id: 'organisation_135_replaced_at_runtime',
-    name: 'Stichting MLC Foundation'
+    name: 'Stichting MLC Foundation',
+    containOrg: null
 },
 {
     id: 'organisation_136_replaced_at_runtime',
-    name: 'Takeda Development Centre Europe Ltd'
+    name: 'Takeda Development Centre Europe Ltd',
+    containOrg: null
 },
 {
     id: 'organisation_137_replaced_at_runtime',
-    name: 'Teknologian tutkimuskeskus VTT Oy'
+    name: 'Teknologian tutkimuskeskus VTT Oy',
+    containOrg: null
 },
 {
     id: 'organisation_138_replaced_at_runtime',
-    name: 'TMF Ð Technologie und Methodenplattform fŸr die Vernetzte Medizinische Forschung e.V.'
+    name: 'TMF Ð Technologie und Methodenplattform fŸr die Vernetzte Medizinische Forschung e.V.',
+    containOrg: null
 },
 {
     id: 'organisation_139_replaced_at_runtime',
-    name: 'UCB Biopharma SPRL'
+    name: 'UCB Biopharma SPRL',
+    containOrg: null
 },
 {
     id: 'organisation_140_replaced_at_runtime',
-    name: 'Universidad Autonoma de Madrid'
+    name: 'Universidad Autonoma de Madrid',
+    containOrg: null
 },
 {
     id: 'organisation_141_replaced_at_runtime',
-    name: 'Universita Degli Studi di Brescia (Italy)'
+    name: 'Universita Degli Studi di Brescia (Italy)',
+    containOrg: null
 },
 {
     id: 'organisation_142_replaced_at_runtime',
-    name: 'UniversitŠtsklinikum Schleswig-Holstein'
+    name: 'UniversitŠtsklinikum Schleswig-Holstein',
+    containOrg: null
 },
 {
     id: 'organisation_143_replaced_at_runtime',
-    name: 'University of Cambridge'
+    name: 'University of Cambridge',
+    containOrg: null
 },
 {
     id: 'organisation_144_replaced_at_runtime',
-    name: 'University of Glasgow'
+    name: 'University of Glasgow',
+    containOrg: null
 },
 {
     id: 'organisation_145_replaced_at_runtime',
-    name: 'University of Limerick'
+    name: 'University of Limerick',
+    containOrg: null
 },
 {
     id: 'organisation_146_replaced_at_runtime',
-    name: 'University of Manchester'
+    name: 'University of Manchester',
+    containOrg: null
 },
 {
     id: 'organisation_147_replaced_at_runtime',
-    name: 'University of Newcastle Upon Tyne'
+    name: 'University of Newcastle Upon Tyne',
+    containOrg: null
 }
 ];
