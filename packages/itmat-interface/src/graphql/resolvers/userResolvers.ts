5032a3601e0a54eb4373c4f0da2e1cbca6dbef5a
Florian Guitton <f.guitton@imperial.ac.uk>
2020-08-25 16:46:20 +0000

    Fixing mongo cursor typing issue

--- packages/itmat-interface/src/graphql/resolvers/userResolvers.ts
+++ packages/itmat-interface/src/graphql/resolvers/userResolvers.ts
@@ -39,7 +39,7 @@ export const userResolvers = {
         getOrganisations: async (__unused__parent: Record<string, unknown>, args: any): Promise<IOrganisation[]> => {
             // everyone is allowed to see all organisations in the app.
             const queryObj = args.organisationId === undefined ? { deleted: null } : { deleted: null, id: args.organisationId };
-            const cursor = db.collections!.organisations_collection.find(queryObj, { projection: { _id: 0 } });
+            const cursor = db.collections!.organisations_collection.find<IOrganisation>(queryObj, { projection: { _id: 0 } });
             return cursor.toArray();
         },
         validateResetPassword: async (__unused__parent: Record<string, unknown>, args: any): Promise<IGenericResponse> => {
