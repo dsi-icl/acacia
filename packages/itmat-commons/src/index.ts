f2f940c11797315db1cfb6fa1ceb83171a763950
sou-chon <sou.chon@outlook.com>
2020-09-21 21:42:16 +0100

    permissions update

--- packages/itmat-commons/src/index.ts
+++ packages/itmat-commons/src/index.ts
@@ -1,10 +1,11 @@
-import { permissions, task_required_permissions } from './permissions';
+import { permissions } from './permissions';
 import * as Models from './models';
-export { permissions, task_required_permissions };
+export { permissions };
 export { GQLRequests } from './graphql';
 export { Models };
 
 export * from './graphql';
 export * from './utils';
 export * from './models';
+export * from './helperFunctions';
 
