4e180f4799c2f6968787b734ee0532fcd09d8a7d
wsy19961129@live.com <wsy19961129@live.com>
2020-11-04 16:33:18 +0000

    remove extra code

--- packages/itmat-interface/src/graphql/resolvers/fileResolvers.ts
+++ packages/itmat-interface/src/graphql/resolvers/fileResolvers.ts
@@ -38,8 +38,6 @@ export const fileResolvers = {
                         if (args.hash === hashString) {
                             resolve(hashString);
                         } else {
-                            console.log(hashString);
-                            console.log(args.hash);
                             return reject(new ApolloError(errorCodes.AUTHENTICATION_ERROR));
                         }
                     });
@@ -82,53 +80,12 @@ export const fileResolvers = {
                                 throw new ApolloError(errorCodes.DATABASE_ERROR);
                             }
                         });
-
                         objStore.uploadFile(stream, args.studyId, fileUri);
                     } catch (e) {
                         Logger.error(errorCodes.FILE_STREAM_ERROR);
                     }
                 });
             });
-            return new Promise<IFile>((resolve, reject) => {
-                try {
-
-                    const stream: Readable = (file as any).createReadStream();
-                    const fileUri = uuid();
-
-                    /* if the client cancelled the request mid-stream it will throw an error */
-                    stream.on('error', (e) => {
-                        Logger.error(e);
-                        reject(new ApolloError(errorCodes.FILE_STREAM_ERROR));
-                    });
-
-                    stream.on('end', async () => {
-                        const fileEntry: IFile = {
-                            id: uuid(),
-                            fileName: file.filename,
-                            studyId: args.studyId,
-                            fileSize: args.fileLength === undefined ? 0 : args.fileLength,
-                            description: args.description,
-                            uploadTime: `${Date.now()}`,
-                            uploadedBy: requester.id,
-                            uri: fileUri,
-                            deleted: null,
-                            hash: ''
-                        };
-
-                        const insertResult = await db.collections!.files_collection.insertOne(fileEntry);
-                        if (insertResult.result.ok === 1) {
-                            resolve(fileEntry);
-                        } else {
-                            throw new ApolloError(errorCodes.DATABASE_ERROR);
-                        }
-                    });
-
-                    objStore.uploadFile(stream, args.studyId, fileUri);
-                } catch (e) {
-                    Logger.error(errorCodes.FILE_STREAM_ERROR);
-                }
-            });
-
         },
         deleteFile: async (__unused__parent: Record<string, unknown>, args: { fileId: string }, context: any): Promise<IGenericResponse> => {
             const requester: Models.UserModels.IUser = context.req.user;
