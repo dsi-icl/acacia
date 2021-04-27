84f17d8afbf18f3469c8e8275b860645b8f84f4c
GuanyuTao <guanyu.tao@outlook.com>
2021-04-27 16:33:50 +0100

    add two new device codes: SMQ and VIR

--- packages/itmat-ui-react/src/components/datasetDetail/tabContent/files/fileTab.tsx
+++ packages/itmat-ui-react/src/components/datasetDetail/tabContent/files/fileTab.tsx
@@ -41,7 +41,9 @@ export const deviceTypes = {
     PSR: 'PSG raw data',
     PSM: 'PSG meta data',
     SMA: 'Stress Monitor App',
-    TFA: 'ThinkFast App'
+    TFA: 'ThinkFast App',
+    SMQ: 'Stress Monitor App Questionnaire',
+    VIR: 'Virtual device type'
 };
 
 const { RangePicker } = DatePicker;
