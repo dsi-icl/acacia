96372cd94bf441608b958fd3696bdd31e4db64da
Florian Guitton <f.guitton@imperial.ac.uk>
2022-10-03 21:37:48 +0100

    fix: Compilation target issue post Nx move

--- packages/itmat-interface/webpack.config.js
+++ packages/itmat-interface/webpack.config.js
@@ -1,6 +1,19 @@
+const path = require('node:path');
+
 const getWebpackConfig = (config) => {
 
-    config.entry.interface = config.entry.main;
+    config.entry.interface = [path.resolve(__dirname, 'src/interfaceServer.ts')];
+
+    config.externals = [{
+        'bcrypt': 'commonjs bcrypt',
+        'express': 'commonjs express',
+        'isobject': 'commonjs isobject',
+        'minio': 'commonjs minio',
+        'mongodb': 'commonjs mongodb',
+        'bufferutil': 'commonjs bufferutil',
+        'utf-8-validate': 'commonjs utf-8-validate',
+        'require_optional': 'commonjs require_optional'
+    }];
 
     return config;
 };
--- packages/itmat-job-executor/webpack.config.js
+++ packages/itmat-job-executor/webpack.config.js
@@ -1,6 +1,19 @@
+const path = require('node:path');
+
 const getWebpackConfig = (config) => {
 
-    config.entry.executor = config.entry.main;
+    config.entry.executor = [path.resolve(__dirname, 'src/jobExecutorServer.ts')];
+
+    config.externals = [{
+        'bcrypt': 'commonjs bcrypt',
+        'express': 'commonjs express',
+        'isobject': 'commonjs isobject',
+        'minio': 'commonjs minio',
+        'mongodb': 'commonjs mongodb',
+        'bufferutil': 'commonjs bufferutil',
+        'utf-8-validate': 'commonjs utf-8-validate',
+        'require_optional': 'commonjs require_optional'
+    }];
 
     return config;
 };
