29d35272a19af6b00608d35cf2b53d0470d07ea8
Florian Guitton <f.guitton@imperial.ac.uk>
2022-10-05 17:55:35 +0100

    fix: Dockerized DMP would service static assets from the wrong location after Nx transition

--- packages/itmat-docker/launcher.js
+++ packages/itmat-docker/launcher.js
@@ -3,7 +3,9 @@ const rateLimit = require('express-rate-limit');
 const compression = require('compression');
 const ITMATInterface = require('./interface').default;
 const ITMATJobExecutor = require('./executor').default;
-const path = require('path');
+const path = require('node:path');
+const fs = require('node:fs');
+const url = require('node:url');
 const config = require('./config/config.json');
 
 let root = express();
@@ -23,19 +25,18 @@ Promise.all([
         root.use('/', router.getApp());
     });
 
-    // Binding static resources folder
-    root.use('/favicon.ico', express.static(path.normalize(`${__dirname}/static/favicon.ico`)));
-    root.use('/manifest.json', express.static(path.normalize(`${__dirname}/static/manifest.json`)));
-    root.use('/static', express.static(path.normalize(`${__dirname}/static`)));
-
     root.use(rateLimit({
         windowMs: 1 * 60 * 1000,
         max: 500
     }));
 
     // Referencing any other requests to the /public/index.html
-    root.use('*', (__unused__req, res) => {
-        res.sendFile(path.resolve('static/index.html'));
+    root.use('*', (req, res) => {
+        const targetFile = path.posix.join(__dirname, url.parse(req.originalUrl).pathname);
+        if (fs.existsSync(targetFile))
+            return res.sendFile(targetFile);
+        else
+            return res.sendFile(path.posix.join(__dirname, 'index.html'));
     });
 
     root.listen(3080, error => {
