584c3905d747ee51705f113fb27621882c02b505
Florian Guitton <f.guitton@imperial.ac.uk>
2023-10-24 11:42:24 +0100

    fix: Remove error message when no job records are found

--- packages/itmat-commons/src/utils/poller.ts
+++ packages/itmat-commons/src/utils/poller.ts
@@ -62,7 +62,8 @@ export class JobPoller {
                 Logger.log(`${this.identity} Finished processing job of type ${updateResult.jobType} - id: ${updateResult.id}.`);
                 this.setInterval();
             } else {
-                Logger.error(`${this.identity} Errored during database update: ${updateResult}`);
+                // Logger.error(`${this.identity} Errored during database update: ${updateResult}`);
+                this.setInterval();
             }
 
         } catch (err) {
